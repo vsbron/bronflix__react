f3bdafbf6e0f1df42b0a82fd597ed15367681805
vsbron <vsbron@gmail.com>
2026-07-18 20:55:23 +0300

    Improve formatting in messages

--- src/features/ai-mode/AIChatMessage.tsx
+++ src/features/ai-mode/AIChatMessage.tsx
@@ -17,9 +17,9 @@ function AIChatMessage({ children, userMessage = false }: AIChatMessageProps) {
     <div
       className={`text-xl max-w-[48%] ${userMessage ? "ml-auto text-right" : ""}`}
     >
-      {userMessage ? "You:" : "BroNflix:"}
+      {userMessage ? "You" : "BroNflix"}:
       <div
-        className={`mt-1 py-4 px-8 text-3xl leading-snug rounded-md text-white relative ${afterClass} ${
+        className={`mt-1 py-6 px-8 text-3xl leading-snug rounded-md text-white relative text-[16px] ${afterClass} ${
           userMessage
             ? "bg-red-900 rounded-br-none after:right-0 after:border-r-[10px] after:border-r-red-900"
             : "bg-stone-800 rounded-bl-none after:left-0 after:border-l-[10px] after:border-l-stone-800"
@@ -28,7 +28,9 @@ function AIChatMessage({ children, userMessage = false }: AIChatMessageProps) {
         {userMessage ? (
           children
         ) : (
-          <ReactMarkdown>{children as string}</ReactMarkdown>
+          <div className="[&>*:first-child]:mt-0 [&>*:last-child]:mb-0">
+            <ReactMarkdown>{children as string}</ReactMarkdown>
+          </div>
         )}
       </div>
     </div>
