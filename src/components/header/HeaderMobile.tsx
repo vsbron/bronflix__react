d9f2ccab6588d4c7625a2d27e27b1d32c0dee4dd
vsbron <vsbron@gmail.com>
2026-07-21 10:39:02 +0300

    Minor UI fox for mobile

--- src/components/header/HeaderMobile.tsx
+++ src/components/header/HeaderMobile.tsx
@@ -5,7 +5,7 @@ import MobileNav from "@/components/header/MobileNav";
 function HeaderMobile() {
   // Returned JSX
   return (
-    <header className="fixed inset-0 bottom-auto px-6 pt-5 pb-10 z-20 flex justify-between items-center bg-header-gradient">
+    <header className="fixed inset-0 bottom-auto px-6 py-5 z-20 flex justify-between items-center bg-header-gradient">
       <LogoFull />
       <Hamburger />
       <MobileNav />
--- src/components/ui/Wrapper.tsx
+++ src/components/ui/Wrapper.tsx
@@ -5,7 +5,7 @@ function Wrapper({ children }: WrapperProps) {
   return (
     <div
       className={
-        "flex flex-col gap-6 sm:gap-10 pl-6 md:pl-0 pr-6 md:pr-12 mt-[5rem] md:mt-0"
+        "flex flex-col gap-6 sm:gap-10 pl-6 md:pl-0 pr-6 md:pr-12 mt-[4.5rem] md:mt-0"
       }
     >
       {children}
